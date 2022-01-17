f42777cbb77d5c800bcc72d65cb9979dd8996659
scllin <scllin@nhri.edu.tw>
2022-01-17 15:12:55 +0800

    version 0.2

--- R/hello.R
+++ R/hello.R
@@ -17,6 +17,40 @@ hello <- function(x) {
   x^2
 }
 
+#CI_mtd: 0:no  1:Delta Method 2:Bootstrap 3:ALL
+Mtd1 <- function(u_delta,se_delta,u_tau,se_tau,cov_deltatau,dat_mrMed,gamma=0.05){ 
+
+	#point estimates
+	IE <- u_tau-u_delta
+	DE <- u_delta
+	rho <- IE/u_tau
+	z=qnorm(1-gamma/2)
+
+	TE_CI <- c(lower=u_tau-z*se_tau,upper=u_tau+z*se_tau)	
+	DE_se=c(NA);IE_se=c(NA);rho_se=c(NA)
+	IE_CI_Delta=c(NA,NA);DE_CI_Delta=c(NA,NA);rho_CI_Delta=c(NA,NA)
+	#IE_CI_boot=c(NA,NA);DE_CI_boot=c(NA,NA);rho_CI_boot=c(NA,NA)
+
+	#===CI: Delta method		
+	#IE_var <- max(se_tau^2 + se_delta^2 - 2*cov_deltatau,(se_tau-se_delta)^2)
+	IE_var <- se_tau^2 + se_delta^2 - 2*cov_deltatau
+	IE_se <- sqrt(IE_var)
+	IE_CI_Delta <- c(lower=IE-z*IE_se,upper=IE+z*IE_se)
+
+	DE_var <- se_delta^2
+	DE_se <- se_delta	
+	DE_CI_Delta <- c(lower=DE-z*DE_se,upper=DE+z*DE_se)
+
+	rho_var <- max(DE_var/u_tau^2 + DE^2*se_tau^2/u_tau^4 - 2*DE*cov_deltatau/u_tau^3,0)
+	rho_se <- sqrt(rho_var)
+	#rho_CI_Delta <- c(lower=rho-z*rho_se,upper=rho+z*rho_se)
+	rho_CI_Delta <- c(rho-z*rho_se,rho+z*rho_se)
+	names(rho_CI_Delta) <- c(paste0(100*(gamma/2),"%"),paste0(100*(1-gamma/2),"%"))
+	
+
+	return(list(TE=u_tau,TE_se=se_tau,TE_CI=TE_CI,DE=DE,DE_se=DE_se,DE_CI=DE_CI_Delta,IE=IE,IE_se=IE_se,IE_CI=IE_CI_Delta,rho=rho,rho_se=rho_se,rho_CI=rho_CI_Delta))
+}
+
 
 M1_IVW <- function(dat_mrMed,gamma=0.05){
 
